187894a1b938dd07340d8681698d90fc2df68897
Daniel Deutsch <deudan1010@gmail.com>
2017-02-23 11:24:40 +0100

    initial

---
+++ src/Searchbar.js
@@ -0,0 +1,14 @@
+export default class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
