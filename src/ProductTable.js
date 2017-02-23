8d053e5dd90e76312518c9968632f5a45af18728
Daniel Deutsch <deudan1010@gmail.com>
2017-02-23 12:30:07 +0100

    update

--- src/ProductTable.js
+++ src/ProductTable.js
@@ -7,7 +7,6 @@ export default class ProductTable extends React.Component {
   render() {
     var rows = [];
     var lastCategory = null;
-    console.log(this.props.inStockOnly)
     this.props.products.forEach((product) => {
       if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
         return;
